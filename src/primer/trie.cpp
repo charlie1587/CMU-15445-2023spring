a9b4b217ab0bd04511d928afdf60f987b72705a3
Charlie Tang <1345308560@qq.com>
2023-09-26 19:01:21 +0800

    完成Trie:Put方法

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -26,6 +26,7 @@ auto Trie::Get(std::string_view key) const -> const T * {
   // 如果当前节点是值节点，返回值
   if (cur->is_value_node_) {
     auto node = dynamic_cast<const TrieNodeWithValue<T> *>(cur.get());
+    return node->value_.get();
   }
   return nullptr;
 }
@@ -37,8 +38,56 @@ auto Trie::Put(std::string_view key, T value) const -> Trie {
 
   // You should walk through the trie and create new nodes if necessary. If the node corresponding to the key already
   // exists, you should create a new `TrieNodeWithValue`.
-  // 创建一个新的Trie和一个指针指向根节点
-
+  //-------------------------------------------------------------------------
+  // 创建一个新的Trie和一个指针指向这个新Trie根节点的共享指针
+  Trie new_trie;
+  std::shared_ptr<TrieNode> new_cur = new_trie.root_;
+  // 初始化new_trie的根节点
+  if (new_cur == nullptr) {
+    new_cur = std::make_shared<TrieNode>();
+  }
+  // 创建一个指针指向根节点
+  std::shared_ptr<const TrieNode> cur = root_;
+  // 判断是不是要新创建节点：0表示不创建，1表示创建
+  int flag = 0;
+  //-------------------------------------------------------------------------
+  // 遍历key
+  for (auto c : key) {
+    // 如果当前节点的孩子中没有c，说明key不存在
+    if (flag||cur->children_.find(c) == cur->children_.end()) {
+      // 创建一个新的孩子节点
+      std::shared_ptr<TrieNode> new_child = std::make_shared<TrieNode>();
+      // 将新孩子节点加入到new_cur的孩子中
+      new_cur->children_.insert(std::make_pair(c, new_child));
+      flag=1;
+    } else {
+      // 拷贝cur指向c对应的孩子节点
+      std::shared_ptr<TrieNode> new_child = cur->children_.at(c)->Clone();
+      // 如果new_child是数值节点，还要拷贝值信息
+      if (new_child->is_value_node_) {
+        auto node = dynamic_cast<const TrieNodeWithValue<T> *>(new_child.get()); // 用dynamic_cast将new_child转换为TrieNodeWithValue<T>类型
+        node->value_ = std::make_shared<T>(value); // 将value拷贝到node->value_中
+        // 将node插入到new_cur的孩子中
+        new_cur->children_.insert(std::make_pair(c, node));
+      }
+      else{
+        // 插入
+        new_cur->children_.insert(std::make_pair(c, new_child));
+      }
+    }
+    // 把cur除了孩子节点外的其他孩子指针都拷贝到new_cur中
+    for (auto &child : cur->children_) {
+      if (child.first != c) {
+        new_cur->children_.insert(std::make_pair(child.first, child.second));
+      }
+    }
+    // 移动cur和new_cur
+    if(!flag)cur = cur->children_.at(c);
+    new_cur = new_cur->children_.at(c);
+  }
+  //-------------------------------------------------------------------------
+  // 返回新建立的Trie
+  return new_trie;
 }
 
 auto Trie::Remove(std::string_view key) const -> Trie {
